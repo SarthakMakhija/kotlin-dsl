8c9f2d8e39ffca560f5f1d66b8c649acd95a32be
sarthak <msarthak@thoughtworks.com>
2017-09-02 23:08:06 +0530

    Sarthak: Initial commit for kotlin-dsl for json builder

---
+++ src/main/kotlin/org/kotlin/dsl/json/JsonDslBuilder.kt
@@ -0,0 +1,47 @@
+package org.kotlin.dsl.json
+
+class Json {
+
+    private lateinit var jsonObject: Obj
+
+    infix fun obj(init: Obj.() -> Unit): Obj {
+        val obj = Obj()
+        obj.init()
+        jsonObject = obj
+
+        return obj
+    }
+
+    fun getValue(key: String) = jsonObject.getValue(key)
+
+    fun render(): String = StringBuilder("{").append("\n").append(jsonObject.renderKeyValue()).append("}").toString()
+}
+
+class Obj {
+
+    private val keyValue = linkedMapOf<String, Any?>()
+
+    infix fun String.to(value: Any?): Unit {
+        keyValue.put(this, value)
+    }
+
+    fun renderKeyValue(): String {
+        val sb = StringBuilder()
+
+        for ( (k, v) in keyValue ){
+            with(sb) {
+                append("$k : $v")
+                append("\n")
+            }
+        }
+        return sb.toString()
+    }
+
+    fun getValue(key: String) = keyValue[key]
+}
+
+fun json(init: Json.() -> Unit): Json {
+    val json = Json()
+    json.init()
+    return json
+}
---
+++ src/test/kotlin/org/kotlin/dsl/json/JsonDlsBuilderUnitTest.kt
@@ -0,0 +1,65 @@
+package org.kotlin.dsl.json
+
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.FunSpec
+
+class JsonDlsBuilderUnitTest : FunSpec() {
+
+    init {
+        test("should add JSON String Value in JSON Object"){
+            val json = json {
+                            obj {
+                                "description" to "Sample"
+                            }
+                        }
+            json.getValue("description") shouldBe "Sample"
+        }
+
+        test("should add JSON Int Value in JSON Object"){
+            val json = json {
+                            obj {
+                                "age" to 12
+                            }
+                        }
+            json.getValue("age") shouldBe 12
+        }
+
+        test("should add JSON Float Value in JSON Object"){
+            val json = json {
+                            obj {
+                                "x-coordinate" to 12.98
+                            }
+                        }
+            json.getValue("x-coordinate") shouldBe 12.98
+        }
+
+        test("should add JSON Array Value in JSON Object"){
+            val countries = arrayOf("India", "US", "UK")
+            val json = json {
+                            obj {
+                                "countries" to countries
+                            }
+                        }
+            json.getValue("countries") shouldBe countries
+        }
+
+        test("should add JSON Boolean Value in JSON Object"){
+            val json = json {
+                            obj {
+                                "active" to true
+                            }
+                        }
+            json.getValue("active") shouldBe true
+        }
+
+        test("should add NULL Value in JSON Object"){
+            val json = json {
+                            obj {
+                                "email" to null
+                            }
+                        }
+            json.getValue("email") shouldBe null
+        }
+    }
+
+}
